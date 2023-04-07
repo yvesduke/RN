2e0c8fcce1c2f0bba7d48ec648ec421031ef4014
Firdous Ali <firdous.ali.tp@gmail.com>
2023-04-07 17:08:09 +0500

    take it out in seperate component

--- App.tsx
+++ App.tsx
@@ -7,53 +7,12 @@
 
 import React from 'react';
 
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
-import ButtonWithFuncComp from './Components/ButtonWithFuncComp';
-import ListwithFuncComp from './Components/ListWithFuncComp';
-import ImageWithFuncComp from './Components/ImageWithFuncComp';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+import HomeScreen from './containers/HomeScreen';
+
 const Stack = createNativeStackNavigator();
-
-const HomeScreen = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <ButtonWithFuncComp bgClr="pink" />
-          <ListwithFuncComp />
-          <ImageWithFuncComp />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
 
 function App(): JSX.Element {
   return (
